045f30f7a2a52cde9c3f3492361a35de835dcf95
sweathannah <sultanatmbashir@gmail.com>
2025-04-16 23:23:59 +0100

    completed auth page

--- sentinel/src/pages/Home.jsx
+++ sentinel/src/pages/Home.jsx
@@ -2,65 +2,47 @@ import Header from '../components/Header';
 
 function Home() {
     return (
-        <section className="relative w-full h-fit">
-        {/* Background Image for large screens */}
-        <div
-            style={{ backgroundImage: "url('/images/waitlist_images/robot_bg.png')" }}
-            className="bg-cover lg:block hidden absolute inset-0 bg-center bg-no-repeat w-full h-full lg:rounded-[3.75rem] rounded-[1.875rem]"
-        ></div>
+        <section className="min-h-screen flex items-center justify-center bg-[#F8F8F8] px-[1.25rem] font-poppins">
+            <div className="flex flex-col items-stretch justify-center md:flex-row bg-white rounded-[1.5rem] shadow-lg p-6 md:p-12 max-w-5xl w-full gap-[1rem]">
+                
+                {/* Left - Image */}
+                <div className="w-[40%] lg:flex hidden justify-center items-center p-4 bg-[#D8D8D8] rounded-[0.5rem] border-[1px] border-solid border-[#D8D8D8]">
+                    <img src="/images/auth_images/Character(i).png" alt="Sentinel Character" className="w-[250px] md:w-[300px]" />
+                </div>
 
-        {/* Background Image for mobile */}
-        <div
-            style={{ backgroundImage: "url('/images/waitlist_images/mobile_bg.png')" }}
-            className="bg-cover block lg:hidden absolute inset-0 bg-center bg-no-repeat w-full h-full lg:rounded-[3.75rem] rounded-[1.875rem]"
-        ></div>
+                {/* Right - Text and Buttons */}
+                <div className="w-full md:w-1/2 flex flex-col justify-centermd:pl-8 bg-white border-[1px] border-solid border-[#E7E7E7] rounded-[0.5rem] py-[2rem] px-[2.5rem]">
+                    <h2 className="text-[1.8rem] font-[700] bg-gradient-to-r from-[#4A3391] to-[#001ECA] bg-clip-text text-transparent mb-[0.7rem]">
+                        Welcome to Sentinel!
+                    </h2>
 
-        {/* Gradient Overlay */}
-        <div className="absolute inset-0 bg-gradient-to-r from-[#4A3391] to-[#001ECA] opacity-50 w-full h-full lg:rounded-[3.75rem] rounded-[1.875rem] lg:block hidden"></div>
+                    <p className="text-[#444444] font-normal text-[0.9rem] mb-[1.7rem]">
+                        Sentinel helps you stay protected with real-time alerts, anonymous incident reporting, and safe-route navigation.
+                    </p>
 
-        {/* Content */}
-        <div className="relative z-10 flex flex-col items-start justify-center h-full text-white text-start max-w-[38.6rem] lg:px-[3.9rem] px-[1rem] lg:py-[5.5rem] py-[4.8rem]">
-            <h1 className="text-[2.7rem] font-[700]">The Future of Campus Security is Here!</h1>
-            <p className="font-[400] text-[1rem] text-[#D7D7D7] mt-[1.5rem]">
-            Join the waitlist and be the first to experience smart, real-time safety.
-            </p>
+                    <button className="bg-[#2545FF] text-[#FFFFFF] text-[0.8rem] py-[0.8rem] rounded-lg font-medium ">
+                        Create account
+                    </button>
+                    <button className="border border-solid border-[#2545FF] text-[#2545FF] text-[0.8rem] py-[0.8rem] rounded-lg font-medium my-[0.8rem]">
+                        Log in to your account
+                    </button>
 
-            <div className="my-[3rem] flex flex-row text-start gap-[0.7rem] items-center">
-            <img src="/images/waitlist_images/student.svg" alt="Registered Students" className="w-[10rem]" />
-            <p className="font-[500] lg:text-[1rem] text-[0.8rem] w-[10rem]">
-                50+ Students Already On Board
-            </p>
+                    <button className="border py-[0.8rem] border-[#AAAAAA] rounded-lg font-medium flex items-center justify-center gap-2 mt-[1.5rem]">
+                        <img src="/images/auth_images/google_icon.svg" alt="Google" className="w-5 h-5" />
+                        Continue with Google
+                    </button>
+
+                    <button className="border font-medium py-[0.8rem] border-[#AAAAAA] rounded-lg flex items-center justify-center gap-2 my-[0.8rem]">
+                        <img src="/images/auth_images/apple_icon.svg" alt="Apple" className="w-5 h-5" />
+                        Continue with Apple
+                    </button>
+
+                    <div className="text-center text-[#515151] text-[1rem] font-[500]">or</div>
+
+                    <button className="bg-[#3FABE1] text-white font-medium text-[0.9rem] py-[0.8rem] mt-[1rem] rounded-lg">Report as a guest</button>
+                </div>
+
             </div>
-
-            {/* Form */}
-            <div>
-            <form action="#" className="flex flex-row gap-[0.9rem] flex-wrap">
-                {/* Input Wrapper */}
-                <label
-                htmlFor="email"
-                className="rounded-full py-[0.8rem] lg:py-[1rem] lg:px-[2.38rem] px-[1.5rem] bg-[#FFFFFF1F]"
-                >
-                <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    placeholder="Enter your Email"
-                    className="bg-transparent lg:w-[12rem] w-[15rem] font-[500] text-[#FFFFFF] text-[0.9rem] 
-                    focus:outline-none focus:ring-0 focus:border-none placeholder:text-[#FFFFFFB3]"
-                />
-                </label>
-
-                {/* Button */}
-                <button
-                className="bg-[#FFFFFF] rounded-full font-[600] text-[#000000] 
-                    lg:text-[0.7rem] text-[0.9rem] py-[0.8rem] lg:py-[1rem] lg:px-[1rem] px-[1.5rem] lg:w-[12rem] w-[18rem]
-                    transition-all duration-300 hover:bg-gradient-to-r hover:from-[#483393] hover:to-[#041FC7] hover:text-white"
-                >
-                Sign up for early access
-                </button>
-            </form>
-            </div>
-        </div>
         </section>
     );
 }
