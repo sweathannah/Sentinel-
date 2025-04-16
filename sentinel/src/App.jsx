c929ef0210738936461e703ddbc788a74c81790b
sweathannah <sultanatmbashir@gmail.com>
2025-04-16 14:38:05 +0100

    setup react app

---
+++ sentinel/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="text-4xl font-bold text-center text-blue-600 mt-10">
+      Hello from Sentinel 🚀
+    </div>
+    </>
+  )
+}
+
+export default App
