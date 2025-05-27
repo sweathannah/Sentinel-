fa36264e2b525253dd781c4ea388f30a9db42d1d
sweathannah <sultanatmbashir@gmail.com>
2025-05-27 13:34:39 +0100

    Refactored folder structure and fixed missing dependencies

--- sentinel/Services/index.js
+++
@@ -1,3 +0,0 @@
-export { authRequests } from './auth/authenticationRequest'
-export { dashboardRequests } from './authorized/dashboardService'
-export { guestsRequests } from './guests/guestsService'
--- sentinel/src/components/index.js
+++
@@ -1,3 +0,0 @@
-// export { default as Footer } from './Footer'
-export { default as GuestReportModal } from './GuestReportModal'
-export { default as Header } from './Header'
